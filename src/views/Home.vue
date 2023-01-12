d70dd8a87fca29ee5d32bab9d61ae58287000155
RonildoLB <45640482+RonildoLB@users.noreply.github.com>
2023-01-12 11:04:38 -0300

    Mudança da tela home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,51 +2,31 @@
   <div id="content">
     <div style="margin: auto">
       <div style="flex-wrap: wrap" class="row">
-        <div style="margin: auto">
-          <div class="component">
-            <div class="interno">
-              <AddHour :id="1"/>
-            </div>
-          </div>
-        </div>
-
-        <div style="margin: auto">
-          <div class="component">
-            <div class="interno">
-              <AddHour :id="2"/>
-            </div>
-          </div>
-        </div>
+        <SelectComponent type="1"/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import AddHour from '../components/AddHour.vue'
+import SelectComponent from './components/SelectComponent.vue'
 
 export default {
-  components: { AddHour },
-    data(){
-      return{
-      }
+  components: { SelectComponent },
+  data() {
+    return {
     }
+  },
+  methods: {
+    
+  }
 }
 </script>
 
 <style>
-  #content {
-    display: flex;
-    width: 100%;
-  }
-  .component {
-    background-image: linear-gradient(to bottom right, #fff3ab 60%, #fff7d2);
-    padding: 1rem;
-    margin: 1rem;
-    box-shadow: 0px 4px 4px 1px #00000025;
-  }
-  .interno {
-    padding: 1rem;
-    border: dashed 1px #8888;
-  }
+#content {
+  display: flex;
+  width: 100%;
+}
 </style>
+
